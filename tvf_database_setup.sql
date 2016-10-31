7c02d8f983e678feeadac09c99b5c3683f3123c1
tauraamui <tauracatamui@gmail.com>
2016-10-31 03:09:52 +0000

    changed users table content

--- tvf_database_setup.sql
+++ tvf_database_setup.sql
@@ -3,8 +3,7 @@ CREATE SCHEMA `tvf` ;
 CREATE TABLE `tvf`.`users` (
   `idusers` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
-  `salt` CHAR(64) NOT NULL,
-  `hash` CHAR(64) NOT NULL,
+  `authhash` CHAR(72) NOT NULL,
   PRIMARY KEY (`idusers`),
   UNIQUE INDEX `idusers_UNIQUE` (`idusers` ASC),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
