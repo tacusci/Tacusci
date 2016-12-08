e271aac43bba438cc4062798338c2f5aa5fa79ab
tauraamui <aplewis95@gmail.com>
2016-12-08 00:11:34 +0000

    refactored sql pass method

--- src/main/kotlin/db/SchemaCreationFile.kt
+++ src/main/kotlin/db/SchemaCreationFile.kt
@@ -21,13 +21,17 @@ class SchemaCreationFile(var file: File) {
     }
 
     private fun passAsSQL() {
-        file.readLines().forEach { line ->
-            val lowerCaseLine = line.toLowerCase()
+        file.readLines().forEach { line -> val lowerCaseLine = line.toLowerCase()
             if (lowerCaseLine.contains("create")) {
                 if (lowerCaseLine.contains("schema")) {
-                    val schemaNameRegex = """([a-zA-Z]+;)""".toRegex()
+                    val schemaNameRegex = """([a-zA-Z]\S+;)""".toRegex()
                     if (schemaNameRegex.containsMatchIn(lowerCaseLine)) {
-                        schemaNameRegex.find(lowerCaseLine)?.groupValues?.forEach { schemas.add(it.replace(";","")) }
+                        val matches = schemaNameRegex.find(lowerCaseLine) ?: return@forEach
+                        if (matches.groups.size > 1) {
+                            matches.groups.forEachIndexed { i, matchGroup ->
+                                if (i > 0) if (matchGroup != null) schemas.add(matchGroup.value.replace(";",""))
+                            }
+                        }
                     }
                 }
             }
