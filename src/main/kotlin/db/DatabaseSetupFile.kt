2df0e3034706f1b1b5399c8fc2e5d72d597989ce
tauraamui <aplewis95@gmail.com>
2016-12-12 08:09:34 +0000

    more comments

--- src/main/kotlin/db/DatabaseSetupFile.kt
+++ src/main/kotlin/db/DatabaseSetupFile.kt
@@ -25,7 +25,7 @@ class DatabaseSetupFile(var file: File) {
     private fun selectSQLSchemas() {
 
         val fileContent = file.readText()
-        //find all instances of a pattern like this: 'CREATE schema tvf;'
+        //find all instances of a pattern like this: CREATE schema tvf;
         val matches = """([a-zA-Z]+)\s([a-zA-Z]+)\s([a-zA-Z]+;)""".toRegex().find(fileContent)
 
         if (matches != null && matches.groupValues.size > 1) {
@@ -35,23 +35,29 @@ class DatabaseSetupFile(var file: File) {
                 if (i == 3) schemas.put(matches.groupValues[3], matches.groupValues[0]) else return@forEach
             }
         }
-
+        //TODO: work out how 'information_schema' is getting added to this list
+        /*
         schemas.forEach { key, value ->
             println(key)
         }
+        */
     }
 
     private fun selectSQLTables() {
         //regex for capturing create table content \(\X*\);
 
         val fileContent = file.readText()
+        //find all instances of pattern like this: CREATE TABLE `tvf`.`groups`
         val matches = """([a-zA-Z]+)\s([a-zA-Z]+)\s(\W[a-zA-Z]+\W).(\W[a-zA-Z]+\W)""".toRegex().find(fileContent)
+
+        var tableName = ""
 
         if (matches != null && matches.groupValues.size > 1) {
             (0..matches.groupValues.size-1).forEach { i ->
                 if (i == 1 && matches.groupValues[i].toLowerCase() != "create") return@forEach
                 if (i == 2 && matches.groupValues[i].toLowerCase() != "table") return@forEach
                 if (i == 3) tables.put(matches.groupValues[3], matches.groupValues[0]) else return@forEach
+                if (i == 4) println(matches.groupValues[4])
                 //need to force include index 4
             }
         }
