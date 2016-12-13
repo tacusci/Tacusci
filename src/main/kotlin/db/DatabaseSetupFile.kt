cd5d34b33e37af1daf0e7dba71076c03a0118c91
tauraamui <tauracatamui@gmail.com>
2016-12-13 07:25:07 +0000

    fixed typo, reapplied build settings

--- src/main/kotlin/db/DatabaseSetupFile.kt
+++ src/main/kotlin/db/DatabaseSetupFile.kt
@@ -32,7 +32,7 @@ class DatabaseSetupFile(var file: File) {
             (0..matches.groupValues.size-1).forEach { i ->
                 if (i == 1 && matches.groupValues[i].toLowerCase() != "create") return@forEach
                 if (i == 2 && matches.groupValues[i].toLowerCase() != "schema") return@forEach
-                if (i == 3) schemas.put(matches.groupValues[3], matches.groupValues[0]) else return@forEach
+                if (i == 3) schemas.put(matches.groupValues[3].replace(";", ""), matches.groupValues[0]) else return@forEach
             }
         }
     }
@@ -47,15 +47,16 @@ class DatabaseSetupFile(var file: File) {
         //find all instances of pattern like this: CREATE TABLE `tvf`.`groups`
         val matches = """([a-zA-Z]+)\s([a-zA-Z]+)\s(\W[a-zA-Z]+\W).(\W[a-zA-Z]+\W)""".toRegex().find(fileContent)
 
-        var tableName = ""
 
         if (matches != null && matches.groupValues.size > 1) {
             (0..matches.groupValues.size-1).forEach { i ->
-                println(i)
                 if (i == 1 && matches.groupValues[i].toLowerCase() != "create") return@forEach
                 if (i == 2 && matches.groupValues[i].toLowerCase() != "table") return@forEach
-                if (i == 3) tables.put(matches.groupValues[3], matches.groupValues[0]) else return@forEach
-                if (i == 4) println(matches.groupValues[4])
+                var tableName = ""
+                if (i == 3) tableName += "'${matches.groupValues[i]}'"
+                tableName += "."
+                if (i == 4) tableName += "'${matches.groupValues[i]}'"
+                println(tableName)
                 //need to force include index 4
             }
         }
