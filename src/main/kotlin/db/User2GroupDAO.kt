97c44e2330c1ff29410d3f589cb9d4b1c3140b57
tauraamui <aplewis95@gmail.com>
2016-12-20 12:49:53 +0000

    started to add DAOs for tables

---
+++ src/main/kotlin/db/User2GroupDAO.kt
@@ -0,0 +1,13 @@
+package db
+
+import java.sql.Connection
+
+/**
+ * Created by alewis on 20/12/2016.
+ */
+class User2GroupDAO(connection: Connection, tableName: String) : DAO(connection, tableName) {
+
+    override fun count(): Int {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
