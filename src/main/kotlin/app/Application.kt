093d63c77f6142bce3692fffd90029463587c739
tauraamui <aplewis95@gmail.com>
2017-01-06 17:36:57 +0000

    removed testing line

--- src/main/kotlin/app/Application.kt
+++ src/main/kotlin/app/Application.kt
@@ -76,8 +76,6 @@ class Application {
         UserHandler.createDefaultUser()
         GroupHandler.addUserToGroup(UserHandler.defaultUser, "admins")
 
-        UserHandler.userDAO.ban("tauraamui")
-
         val layoutTemplate = "/templates/layout.vtl"
 
         logger.info("Setting port to $port")
