07497a61e8974b43591547a786b977b44cfae9c9
tauraamui <aplewis95@gmail.com>
2017-01-31 12:16:08 +0000

    build is faster

--- src/main/kotlin/handlers/GroupHandler.kt
+++ src/main/kotlin/handlers/GroupHandler.kt
@@ -47,7 +47,6 @@ object GroupHandler : KLogging() {
     val groupDAO: GroupDAO = DAOManager.getDAO(DAOManager.TABLE.GROUPS) as GroupDAO
 
     fun createGroup(group: Group): Boolean {
-        println("some generic change")
         if (!group.isValid()) return false
         groupDAO.insertGroup(group)
         return true
