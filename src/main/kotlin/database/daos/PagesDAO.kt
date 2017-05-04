8d67dcf23163605133079f55d8df3acd12a6f972
tauraamui <aplewis95@gmail.com>
2017-05-04 12:10:03 +0100

    create PageDAO

---
+++ src/main/kotlin/database/daos/PagesDAO.kt
@@ -0,0 +1,58 @@
+/*
+ * # DON'T BE A DICK PUBLIC LICENSE
+ *
+ * > Version 1.1, December 2016
+ *
+ * > Copyright (C) 2016 Adam Prakash Lewis
+ *
+ *  Everyone is permitted to copy and distribute verbatim or modified
+ *  copies of this license document.
+ *
+ * > DON'T BE A DICK PUBLIC LICENSE
+ * > TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  1. Do whatever you like with the original work, just don't be a dick.
+ *
+ *      Being a dick includes - but is not limited to - the following instances:
+ *
+ * 	 1a. Outright copyright infringement - Don't just copy this and change the name.
+ * 	 1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
+ * 	 1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
+ *
+ *  2. If you become rich through modifications, related works/services, or supporting the original work,
+ *  share the love. Only a dick would make loads off this work and not buy the original work's
+ *  creator(s) a pint.
+ *
+ *  3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes
+ *  you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
+ */
+
+package database.daos
+
+import mu.KLogging
+import java.sql.SQLException
+import java.util.*
+
+/**
+ * Created by alewis on 04/05/2017.
+ */
+class PagesDAO(url: String, dbProperties: Properties, tableName: String) : GenericDAO(url, dbProperties, tableName) {
+
+    companion object : KLogging()
+
+    fun getPageID(pageTitle: String): Int {
+        connect()
+        var pageId = -1
+        try {
+            val selectStatement = "SELECT PAGE_ID FROM $tableName WHERE PAGE_TITLE=?"
+            val preparedStatement = connection?.prepareStatement(selectStatement)
+            preparedStatement?.setString(1, pageTitle)
+            val resultSet = preparedStatement?.executeQuery()
+            if (resultSet!!.next()) {
+                pageId = resultSet.getInt(1)
+            }
+            disconnect()
+        } catch (e: SQLException) { logger.error(e.message); disconnect() }
+        return pageId
+    }
+}
