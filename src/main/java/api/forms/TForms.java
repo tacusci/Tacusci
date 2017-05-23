6b835bc11cc1b4400ec5ef2f2a998771daf590eb
tauraamui <aplewis95@gmail.com>
2017-05-23 12:04:53 +0100

    refactor/rename

--- src/main/java/api/forms/TForms.java
+++ src/main/java/api/forms/TForms.java
@@ -1,5 +1,6 @@
 package api.forms;
 
+import app.basecontrollers.Web;
 import spark.Request;
 import utils.j2htmlPartials;
 
@@ -18,5 +19,9 @@ public class TForms {
 
     public String getSignOutForm() {
         return j2htmlPartials.INSTANCE.pureMenuItemForm(request.session(), "sign_out_form", "/login", "post", "Logout").render();
+    }
+
+    public String getFormHash(String formName) {
+        return Web.INSTANCE.getFormHash(request.session(), formName);
     }
 }
