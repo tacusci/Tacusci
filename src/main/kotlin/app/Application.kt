134a9769df59da5688466e973d4848970395e9d5
tauraamui <aplewis95@gmail.com>
2017-05-31 08:35:00 +0100

    spelling correction

--- src/main/kotlin/app/Application.kt
+++ src/main/kotlin/app/Application.kt
@@ -166,7 +166,7 @@ fun main(args: Array<String>) {
     CliOptions.cliOptions.addAll(listOf(CliOption("Username", "username", true),
             CliOption("Password", "password", true),
             CliOption("Debug Mode", "debug", false),
-            CliOption("Disable vibose output in debug mode", "disable_debug_output", false)))
+            CliOption("Disable verbose output in debug mode", "disable_debug_output", false)))
     CliOptions.parseArgs(args)
 
     Config.load()
