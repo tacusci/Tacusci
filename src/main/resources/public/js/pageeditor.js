de81e962a98d76c0cfa63d3dd38becccc94fd100
tauraamui <aplewis95@gmail.com>
2017-06-13 19:07:24 +0100

    disable temp HTML 5 local storeage existance check

--- src/main/resources/public/js/pageeditor.js
+++ src/main/resources/public/js/pageeditor.js
@@ -1,9 +1,10 @@
-
+/*
 if (typeof(Storage) !== "undefined") {
     alert("HTML 5 Local Storage is supported")
 } else {
     alert("HTML 5 Local Storage not supported")
 }
+*/
 
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
