2a7e40f3d0a83743614d7373dff9bcc5c67f557f
tauraamui <tauracatamui@gmail.com>
2017-06-27 01:24:38 +0100

    work on overhauling user management

--- src/main/kotlin/app/core/controllers/UserManagementController.kt
+++ src/main/kotlin/app/core/controllers/UserManagementController.kt
@@ -79,6 +79,7 @@ class UserManagementController : Controller {
 
         val userAdminForm = genUserForm(request)
         //model.put("user_admin_form", userAdminForm.render())
+        model.put("user_management_changes_made", true)
         return ModelAndView(model, layoutTemplate)
     }
 
