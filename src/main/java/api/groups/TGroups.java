1a477162f7a8b2c635676921e01beda7cdc1e3ff
tauraamui <aplewis95@gmail.com>
2017-06-29 18:19:31 +0100

    work on group management

--- src/main/java/api/groups/TGroups.java
+++ src/main/java/api/groups/TGroups.java
@@ -2,9 +2,12 @@ package api.groups;
 
 import api.core.TAPIClass;
 import app.core.core.handlers.GroupHandler;
+import database.models.Group;
 import database.models.User;
 import spark.Request;
 import spark.Response;
+
+import java.util.List;
 
 /**
  * Created by tauraaamui on 19/06/2017.
@@ -20,4 +23,6 @@ public class TGroups extends TAPIClass {
     public boolean groupExists(String groupName) { return GroupHandler.INSTANCE.groupExists(groupName); }
     public boolean userInGroup(User user, String groupName) { return GroupHandler.INSTANCE.userInGroup(user, groupName); }
     public boolean userInGroup(String username, String groupName) { return GroupHandler.INSTANCE.userInGroup(username, groupName); }
+
+    public List<Group> getGroups() { return GroupHandler.INSTANCE.getGroupDAO().getGroups(); }
 }
