6f6f52806491206752994d898debbb5986fd80e4
tauraamui <aplewis95@gmail.com>
2017-12-18 17:26:05 +0000

    include passed by Velocity before insertion

--- src/main/java/api/includes/TIncludes.java
+++ src/main/java/api/includes/TIncludes.java
@@ -2,6 +2,7 @@ package api.includes;
 
 import api.core.TAPIClass;
 import app.core.handlers.IncludeHandler;
+import app.core.pages.includes.IncludeController;
 import database.models.Include;
 import spark.Request;
 import spark.Response;
@@ -13,7 +14,7 @@ public class TIncludes extends TAPIClass {
     public TIncludes(Request request, Response response) { super(request, response); }
 
     public List<Include> getAllIncludes() { return IncludeHandler.INSTANCE.getAllIncludes(); }
-    public String getInclude(String includeTitle) { return getIncludeByTitle(includeTitle).getContent(); }
+    public String getInclude(String includeTitle) { return IncludeController.INSTANCE.renderInclude(getIncludeByTitle(includeTitle), request, response); }
     public Include getIncludeByTitle(String includeTitle) { return IncludeHandler.INSTANCE.getIncludeByTitle(includeTitle); }
     public Include getIncludeById(Integer includeId) { return IncludeHandler.INSTANCE.getIncludeById(includeId); }
 }
