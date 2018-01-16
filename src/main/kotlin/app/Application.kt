711b51213358a38aed6d6b8b2559fe8f42ec4cc4
tauraamui <aplewis95@gmail.com>
2018-01-16 11:43:07 +0000

    should not have removed that oops

--- src/main/kotlin/app/Application.kt
+++ src/main/kotlin/app/Application.kt
@@ -108,6 +108,8 @@ class Application {
         if (maxThreads > 0) threadPool(maxThreads)
         if (maxThreads > 0 && minThreads > 0 && threadIdleTimeout > 0) threadPool(maxThreads, minThreads, threadIdleTimeout)
 
+        staticFiles.location("/public")
+        staticFiles.expireTime(600L)
         setupSparkRoutes()
     }
 
