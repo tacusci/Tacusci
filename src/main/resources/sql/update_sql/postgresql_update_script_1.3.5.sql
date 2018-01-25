6f16a731365cfcae278ec9558a242661ce2f0300
tauraamui <aplewis95@gmail.com>
2018-01-25 17:55:38 +0000

    work on implement page management list query dropdown

--- src/main/resources/sql/update_sql/postgresql_update_script_1.3.5.sql
+++ src/main/resources/sql/update_sql/postgresql_update_script_1.3.5.sql
@@ -1,4 +1,4 @@
-ALTER TABLE $schema_name.pages ADD COLUMN IF NOT EXISTS disabled BOOLEAN NOT NULL DEFAULT false
+ALTER TABLE $schema_name.pages ADD COLUMN IF NOT EXISTS disabled BOOLEAN NOT NULL DEFAULT false;
 
 CREATE SEQUENCE IF NOT EXISTS $schema_name.sql_query_collection_seq;
 
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS $schema_name.sql_query_collection (
     id_sql_query_collection INTEGER PRIMARY KEY UNIQUE NOT NULL DEFAULT NEXTVAL ('$schema_name.sql_query_collection_seq'),
     created_date_time TEXT NOT NULL,
     last_updated_date_time TEXT NOT NULL,
+    query_label VARCHAR(45) UNIQUE NOT NULL,
     query_name VARCHAR(45) UNIQUE NOT NULL,
     query_text TEXT
 );
